8f43f719bc9c1e535c9448b9aebee1cfc71e0d15
Chris Zhao <xinyue.zhao@immonet.de>
2017-07-02 16:43:45 +0200

    Added foreach for normal output on list

--- src/main.kt
+++ src/main.kt
@@ -1,6 +1,17 @@
-/**
- * Created by xzhao on 02.07.17.
- */
 fun main(args: Array<String>) {
-    println("start lambda-world")
+    println("Examples ....")
+    println()
+
+    val list: ArrayList<String> = ArrayList()
+    list.add("one")
+    list.add("two")
+    list.add("three")
+    foreachList(
+            list
+    )
+}
+
+fun foreachList(vals: List<String>) {
+    println("[foreach] on list")
+    vals.forEach({ s -> println(s) })
 }
