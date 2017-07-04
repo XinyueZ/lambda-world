d12a75cb7c5155a2a43a95c6f5b4ef69975d94c4
Chris Zhao <xinyue.zhao@immonet.de>
2017-07-04 21:30:01 +0200

    Optimized lambda

--- src/main.kt
+++ src/main.kt
@@ -9,22 +9,23 @@ fun main(args: Array<String>) {
     translateNumbers(list2)
     sum(list2)
     sumParallel(list2)
+    sumAdvanced(list2)
 }
 
 fun foreachList(vals: List<String>) {
     println("[foreach] on list")
-    vals.forEach({ s -> println(s) })
+    vals.forEach({ println(it) })
 }
 
 fun streamForeachList(vals: List<String>) {
     println("[stream.foreach] on list")
-    vals.stream().forEach({ s -> println(s) })
+    vals.stream().forEach({ println(it) })
 }
 
 fun translateNumbers(vals: List<String>) {
     println("[stream.mapToInt.foreach] on list")
-    vals.stream().mapToInt { value: String? ->
-        when (value) {
+    vals.stream().mapToInt {
+        when (it) {
             "one" -> 1
             "two" -> 2
             "three" -> 3
@@ -33,7 +34,7 @@ fun translateNumbers(vals: List<String>) {
             "six" -> 6
             else -> -1
         }
-    }.forEach { s -> println(s) }
+    }.forEach { println(it) }
 }
 
 fun sum(vals: List<String>) {
@@ -41,8 +42,8 @@ fun sum(vals: List<String>) {
     vals.stream().filter({
         if (vals.indexOf(it) == vals.size - 1) print(it + " = ") else print(it + " + ")
         true
-    }).mapToInt { value: String? ->
-        when (value) {
+    }).mapToInt {
+        when (it) {
             "one" -> 1
             "two" -> 2
             "three" -> 3
@@ -51,17 +52,16 @@ fun sum(vals: List<String>) {
             "six" -> 6
             else -> -1
         }
-    }.sum().let { println(it.toString()) }
+    }.sum().let { println(it) }
 }
-
 
 fun sumParallel(vals: List<String>) {
     println("[stream.mapToInt.parallel.sum] on list")
     vals.stream().parallel().filter({
         if (vals.indexOf(it) == vals.size - 1) print(it + " = ") else print(it + " + ")
         true
-    }).mapToInt { value: String? ->
-        when (value) {
+    }).mapToInt {
+        when (it) {
             "one" -> 1
             "two" -> 2
             "three" -> 3
@@ -70,25 +70,28 @@ fun sumParallel(vals: List<String>) {
             "six" -> 6
             else -> -1
         }
-    }.sum().let { println(it.toString()) }
+    }.sum().let { println(it) }
 }
-
-
 
 //fun sumAdvanced(vals: List<String>) {
 //    println("[stream.mapToInt.toList] on list")
-//    vals.stream().mapToInt { value: String? ->
-//        when (value) {
-//            "one" -> 1
-//            "two" -> 2
-//            "three" -> 3
-//            "four" -> 4
-//            "five" -> 5
-//            "six" -> 6
-//            else -> -1
-//        }
-//    }.toList().stream().filter({
-//        it
-//        true
-//    }).sum().let { println(it.toString()) }
+//
+//    vals.stream().mapToInt { }
+//
+////     vals.stream().mapToInt { value: String? ->
+////        when (value) {
+////            "one" -> 1
+////            "two" -> 2
+////            "three" -> 3
+////            "four" -> 4
+////            "five" -> 5
+////            "six" -> 6
+////            else -> -1
+////        }
+////    }
+//
+////    nums.stream().filter({
+////        if (nums.indexOf(it) == nums.size - 1) print(it.toString() + " = ") else print(it.toString() + " + ")
+////        true
+////    }).mapToInt { it }.sum().let { println(it.toString()) }
 //}
