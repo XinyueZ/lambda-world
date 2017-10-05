7ce1ee03d338c36925e92a194a466e37256d15cb
Chris Zhao <xinyue.zhao@immonet.de>
2017-10-05 16:27:17 +0200

    Added log

--- coroutine/src/main1.kt
+++ coroutine/src/main1.kt
@@ -10,6 +10,9 @@ import retrofit2.http.GET
 import java.util.concurrent.TimeUnit
 import kotlin.system.measureTimeMillis
 
+fun log(msg: String) = print("[${Thread.currentThread().name}: ] $msg")
+fun logln(msg: String) = println("[${Thread.currentThread().name}: ] $msg")
+
 const val Time1 = 5L
 const val Time2 = 5L
 val sumTime = Time1 + Time2
@@ -19,25 +22,25 @@ fun add(x: Int, y: Int) = x + y
 fun main(args: Array<String>) = runBlocking<Unit> {
     //join:  wait until launch child routine completes
     //aWait: wait until async child routine completes
-    measureTimeMillis { doSomethingAsync(false).apply { join() } }.apply { println("Completed noDependency in $this ms") }
-    measureTimeMillis { doSomethingAsync(true).apply { join() } }.apply { println("Completed dependency in $this ms") }
-    measureTimeMillis { networkCall().apply { join() } }.apply { println("Completed networkCall in $this ms") }
-    measureTimeMillis { networkCallAsync().apply { join() } }.apply { println("Completed networkCallAsync in $this ms") }
+    measureTimeMillis { doSomethingAsync(false).apply { join() } }.apply { logln("Completed noDependency in $this ms") }
+    measureTimeMillis { doSomethingAsync(true).apply { join() } }.apply { logln("Completed dependency in $this ms") }
+    measureTimeMillis { networkCall().apply { join() } }.apply { logln("Completed networkCall in $this ms") }
+    measureTimeMillis { networkCallAsync().apply { join() } }.apply { logln("Completed networkCallAsync in $this ms") }
     measureTimeMillis {
         repeatFunction().apply {
-            println("main: I'm tired of waiting!")
+            logln("main: I'm tired of waiting!")
             delay(1300) // Without this the application goes end, because coroutine only suspend not blocking.
             cancelAndJoin() // Must do here, otherwise output of this repeatFunction going on.
-            println("done, the repeat")
+            logln("done, the repeat")
         }
     }.apply {
-        println("Completed repeatFunction in $this ms")
+        logln("Completed repeatFunction in $this ms")
     }
     measureTimeMillis { repeatUnderTimer() } // Not do join() so that output shows behind next codes.
 
     launch {
         println()
-        println("Last 30 sec wait for all jobs if possible")
+        logln("Last 30 sec wait for all jobs if possible")
         delay(30, TimeUnit.SECONDS)
     }.apply { join() }
 }
@@ -47,22 +50,22 @@ fun doSomethingAsync(depend: Boolean) = when (depend) {
     true -> dependency()
 }
 
-fun noDependency() = launch {
-    println("Call two functions which don't depend each other, pretend calling on the remote server, wait for some minutes less than $sumTime seconds........")
+fun noDependency() = launch(CommonPool) {
+    logln("Call two functions which don't depend each other, pretend calling on the remote server, wait for some minutes less than $sumTime seconds........")
 
     val num1 = asyncGetNum1()
     val num2 = asyncGetNum2()
-    println("The answer: ${add(num1.await(), num2.await())}")
-    println("Finish computing")
+    logln("The answer: ${add(num1.await(), num2.await())}")
+    logln("Finish computing")
 }
 
-fun dependency() = launch {
-    println("Call two functions which depend each other, need about $sumTime  seconds.......")
+fun dependency() = launch(CommonPool) {
+    logln("Call two functions which depend each other, need about $sumTime  seconds.......")
 
     val num1 = getNum1()
     val num2 = getNum2()
     val addedResult = add(num1, num2)
-    println("The answer: $addedResult")
+    logln("The answer: $addedResult")
 }
 
 internal data class Hello(@field:SerializedName("id") val id: Int, @field:SerializedName("content") val content: String)
@@ -73,14 +76,14 @@ internal interface Service {
 
 }
 
-fun networkCall() = launch {
-    println("Call some feeds normally, it needs some sec......")
+fun networkCall() = launch(CommonPool) {
+    logln("Call some feeds normally, it needs some sec......")
 
     val response = getResponse()
     response.takeIf { it.isSuccessful }?.let {
-        println("response networkCall: ${it.body()}")
+        logln("response networkCall: ${it.body()}")
     } ?: kotlin.run {
-        println("Something wrong at getting response")
+        logln("Something wrong at getting response")
     }
 
     // Because JVM might end before response coming. We could miss the output.
@@ -91,40 +94,42 @@ fun networkCall() = launch {
     // Or: join() to wait until the child routine completes.
 }
 
-fun networkCallAsync() = launch {
-    println("Call some feeds with async, it needs some sec......")
+fun networkCallAsync() = launch(CommonPool) {
+    logln("Call some feeds with async, it needs some sec......")
 
     async(CommonPool) {
         val response = getResponse()
         response.takeIf { it.isSuccessful }?.let {
-            println("response networkCallAsync: ${it.body()}")
+            logln("response networkCallAsync: ${it.body()}")
         } ?: kotlin.run {
-            println("Something wrong at getting response")
+            logln("Something wrong at getting response")
         }
     }.apply { await() }
 }
 
-fun repeatFunction() = launch {
+fun repeatFunction() = launch(CommonPool) {
+    logln("Do something repeatably......")
+
     try {
-        println("Try to play with kotlin builtIn repeat()")
+        logln("Try to play with kotlin builtIn repeat()")
         val startTime = System.currentTimeMillis()
         var nextPrintTime = startTime
         var i = 0
         while (isActive) { // computation loop, just wastes CPU
             // print a message twice a second
             if (System.currentTimeMillis() >= nextPrintTime) {
-                println("I'm sleeping ${i++} ...")
+                logln("I'm sleeping ${i++} ...")
                 nextPrintTime += 500L
             }
         }
     } finally {
-        println("finally in repeat")
+        logln("finally in repeat")
     }
 }
 
-fun repeatUnderTimer() = launch {
+fun repeatUnderTimer() = launch(CommonPool) {
     withTimeoutOrNull(10, TimeUnit.SECONDS) {
-        println("Doing under 10 sec control...")
+        logln("Doing under 10 sec control...")
 
         repeat(Int.MAX_VALUE) {
             print("[$it]:")
@@ -137,31 +142,31 @@ fun repeatUnderTimer() = launch {
         }
     }.takeIf { it == null }.apply {
         println()
-        println("10 sec job done!")
+        logln("10 sec job done!")
     }
 }
 
 private fun getNum1() = 50.apply {
     TimeUnit.SECONDS.sleep(Time1)
-    println("$Time1 sec call")
+    logln("$Time1 sec call")
 }
 
 private fun getNum2() = 50.apply {
     TimeUnit.SECONDS.sleep(Time2)
-    println("$Time2 sec call")
+    logln("$Time2 sec call")
 }
 
 private fun asyncGetNum1() = async(CommonPool) {
     50.apply {
         delay(Time1, TimeUnit.SECONDS)
-        println("$Time1 sec call")
+        logln("$Time1 sec call")
     }
 }
 
 private fun asyncGetNum2() = async(CommonPool) {
     50.apply {
         delay(Time2, TimeUnit.SECONDS)
-        println("$Time2 sec call")
+        logln("$Time2 sec call")
     }
 }
 
