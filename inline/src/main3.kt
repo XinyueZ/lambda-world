fa53a43b67a687ea6ea47ffd2c407587141887c9
Chris Zhao <xinyue.zhao@immonet.de>
2017-10-25 00:08:42 +0200

    Added takeUnless demo

--- inline/src/main3.kt
+++ inline/src/main3.kt
@@ -2,10 +2,11 @@ import java.util.concurrent.TimeUnit
 
 fun main(args: Array<String>) {
     takeIf()
+    takeUnless()
 }
 
 fun takeIf() {
-    logln("Demo how to use [takeIf]")
+    logln("Demo how to use [takeIf], the if-then")
 
     val listRandom = List(100) {
         TimeUnit.MILLISECONDS.sleep(60)
@@ -19,6 +20,25 @@ fun takeIf() {
             logln("Even: $it")
         } ?: run {
             logln("Odd: $it")
+        }
+    }
+}
+
+fun takeUnless() {
+    logln("Demo how to use [takeUnless], the if-not-then, the result is contrary to [takeIf]")
+
+    val listRandom = List(100) {
+        TimeUnit.MILLISECONDS.sleep(60)
+        System.currentTimeMillis()
+    }
+
+    listRandom.forEach {
+        it.takeUnless {
+            it % 2 == 0L
+        }?.let {
+            logln("Odd: $it")
+        } ?: run {
+            logln("Even: $it")
         }
     }
 }
