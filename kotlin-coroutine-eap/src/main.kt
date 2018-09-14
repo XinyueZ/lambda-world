5afc31105dea8bb5e76b9c054a87a8c4e7340e58
XinyueZ <xinyue.zhao@immonet.de>
2018-09-14 13:14:36 +0200

    [SECTION]Waiting for an other job, do next until it being finished
    https://sourcegraph.com/github.com/Kotlin/kotlinx.coroutines@d1be1c9d970e29fcc177bb3767087af48935d400/-/blob/coroutines-guide.md#waiting-for-a-job

--- kotlin-coroutine-eap/src/main.kt
+++ kotlin-coroutine-eap/src/main.kt
@@ -6,6 +6,7 @@ import kotlinx.coroutines.runBlocking
 fun main(args: Array<String>) {
     //impatientWait()
     //patientWait()
+    patientWaitUntilOtherFinish()
 }
 
 //https://sourcegraph.com/github.com/Kotlin/kotlinx.coroutines@d1be1c9d970e29fcc177bb3767087af48935d400/-/blob/coroutines-guide.md#bridging-blocking-and-non-blocking-worlds
@@ -30,4 +31,16 @@ fun patientWait() = runBlocking {
     }
     println("I am first, I want to wait: $patientWait.")
     delay(patientWait)
+}
+
+//https://sourcegraph.com/github.com/Kotlin/kotlinx.coroutines@d1be1c9d970e29fcc177bb3767087af48935d400/-/blob/coroutines-guide.md#waiting-for-a-job
+fun patientWaitUntilOtherFinish() = runBlocking  {
+    val bkTaskDuration = 5000L
+    val other = GlobalScope.launch {
+        delay(bkTaskDuration)
+        println("impatientWait background coroutine with GlobalScope.launch.")
+    }
+    println("I am first, I want to wait until other finishing.")
+    // main coroutine (runBlocking) is not tied to the duration of the background job in any way.
+    other.join()
 }
