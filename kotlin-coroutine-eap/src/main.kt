92c684cb1a3e3b6fe956595e496a83aa4fe6763b
XinyueZ <xinyue.zhao@immonet.de>
2018-09-14 11:34:42 +0200

    [SECTION]Bridging blocking and non-blocking worlds
    https://sourcegraph.com/github.com/Kotlin/kotlinx.coroutines@d1be1c9d970e29fcc177bb3767087af48935d400/-/blob/coroutines-guide.md#bridging-blocking-and-non-blocking-worlds

--- kotlin-coroutine-eap/src/main.kt
+++ kotlin-coroutine-eap/src/main.kt
@@ -1,3 +1,33 @@
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
 fun main(args: Array<String>) {
-    println("hello,world")
+    //impatientWait()
+    //patientWait()
+}
+
+//https://sourcegraph.com/github.com/Kotlin/kotlinx.coroutines@d1be1c9d970e29fcc177bb3767087af48935d400/-/blob/coroutines-guide.md#bridging-blocking-and-non-blocking-worlds
+fun impatientWait() = runBlocking {
+    val bkTaskDuration = 5000L
+    val impatientWait = 1000L
+    GlobalScope.launch {
+        delay(bkTaskDuration)
+        println("impatientWait background coroutine with GlobalScope.launch.")
+    }
+    println("I am first, I want to wait: $impatientWait.")
+    delay(impatientWait)
+}
+
+//https://sourcegraph.com/github.com/Kotlin/kotlinx.coroutines@d1be1c9d970e29fcc177bb3767087af48935d400/-/blob/coroutines-guide.md#bridging-blocking-and-non-blocking-worlds
+fun patientWait() = runBlocking {
+    val bkTaskDuration = 5000L
+    val patientWait = 5000L * 5
+    GlobalScope.launch {
+        delay(bkTaskDuration)
+        println("impatientWait background coroutine with GlobalScope.launch.")
+    }
+    println("I am first, I want to wait: $patientWait.")
+    delay(patientWait)
 }
