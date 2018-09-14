21d8992e7b8798a6a5c2ed5efe12e1b4bc541e3f
XinyueZ <xinyue.zhao@immonet.de>
2018-09-14 14:21:42 +0200

    [SECTION]Cancel exection, you can wait until rest done after cancel
    https://sourcegraph.com/github.com/Kotlin/kotlinx.coroutines@0.26.0-eap13/-/blob/coroutines-guide.md#cancelling-coroutine-execution

--- kotlin-coroutine-eap/src/main.kt
+++ kotlin-coroutine-eap/src/main.kt
@@ -9,7 +9,8 @@ fun main(args: Array<String>) {
     //patientWait()
     //patientWaitUntilOtherFinish()
     //patientWaitUntilChildFinish()
-    patientWaitUntilChildInOtherScopeFinish()
+    //patientWaitUntilChildInOtherScopeFinish()
+    patientWaitUntilChildInOtherScopeCancelled()
 }
 
 //https://sourcegraph.com/github.com/Kotlin/kotlinx.coroutines@d1be1c9d970e29fcc177bb3767087af48935d400/-/blob/coroutines-guide.md#bridging-blocking-and-non-blocking-worlds
@@ -78,6 +79,37 @@ fun patientWaitUntilChildInOtherScopeFinish() = runBlocking {
             println("receiving...")
         }
         println("The child in the new scope.")
+    }
+
+    println("I am first, I want to wait until my child being finishing.")
+    //An outer coroutine (runBlocking in our example) does not complete until all the coroutines launched in its scope complete.
+    //The main difference between runBlocking and coroutineScope is that the latter does not block the current thread while waiting for all children to complete.
+}
+
+//https://sourcegraph.com/github.com/Kotlin/kotlinx.coroutines@0.26.0-eap13/-/blob/coroutines-guide.md#cancelling-coroutine-execution
+fun patientWaitUntilChildInOtherScopeCancelled() = runBlocking {
+    val bkTaskDuration = 1000L
+    val newScopeBkTaskDuration = 20000L
+    val cancelDuration = 3000L
+
+    launch {
+        //This is my child(runBlocking main)
+        delay(bkTaskDuration)
+        println("messaging...")
+    }
+
+    coroutineScope { //An other long-time blocking which doesn't complete until child finishes.
+        //A new scope which blocks main(runBlocking).
+        val child = this.launch {
+            //A child of the new scope
+            delay(newScopeBkTaskDuration)
+            println("receiving...")
+        }
+        println("The child in the new scope.")
+        delay(cancelDuration)
+        child.cancel()
+        child.join()
+        println("I have no patient, cancel this child.")
     }
 
     println("I am first, I want to wait until my child being finishing.")
