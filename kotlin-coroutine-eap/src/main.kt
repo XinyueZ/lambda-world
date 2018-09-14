bbb14247e321f9f76f5b0333d84ccd5cd83e6844
XinyueZ <xinyue.zhao@immonet.de>
2018-09-14 15:30:44 +0200

    [SECTION]Making heavy long computation cancellable
    https://sourcegraph.com/github.com/Kotlin/kotlinx.coroutines@0.26.0-eap13/-/blob/coroutines-guide.md#making-computation-code-cancellable

--- kotlin-coroutine-eap/src/main.kt
+++ kotlin-coroutine-eap/src/main.kt
@@ -1,8 +1,12 @@
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.NonCancellable
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 
 fun main(args: Array<String>) {
     //impatientWait()
@@ -10,7 +14,8 @@ fun main(args: Array<String>) {
     //patientWaitUntilOtherFinish()
     //patientWaitUntilChildFinish()
     //patientWaitUntilChildInOtherScopeFinish()
-    patientWaitUntilChildInOtherScopeCancelled()
+    //patientWaitUntilChildInOtherScopeCancelled()
+    patientWaitUntilCancelHeavyJob()
 }
 
 //https://sourcegraph.com/github.com/Kotlin/kotlinx.coroutines@d1be1c9d970e29fcc177bb3767087af48935d400/-/blob/coroutines-guide.md#bridging-blocking-and-non-blocking-worlds
@@ -71,7 +76,8 @@ fun patientWaitUntilChildInOtherScopeFinish() = runBlocking {
         println("messaging...")
     }
 
-    coroutineScope {//An other long-time blocking which doesn't complete until child finishes.
+    coroutineScope {
+        //An other long-time blocking which doesn't complete until child finishes.
         //A new scope which blocks main(runBlocking).
         this.launch {
             //A child of the new scope
@@ -98,9 +104,10 @@ fun patientWaitUntilChildInOtherScopeCancelled() = runBlocking {
         println("messaging...")
     }
 
-    coroutineScope { //An other long-time blocking which doesn't complete until child finishes.
+    coroutineScope {
+        //An other long-time blocking which doesn't complete until child finishes.
         //A new scope which blocks main(runBlocking).
-        val child = this.launch {
+        val child = this.launch(Dispatchers.Default)  {
             //A child of the new scope
             delay(newScopeBkTaskDuration)
             println("receiving...")
@@ -108,6 +115,51 @@ fun patientWaitUntilChildInOtherScopeCancelled() = runBlocking {
         println("The child in the new scope.")
         delay(cancelDuration)
         child.cancel()
+        child.join()
+        println("I have no patient, cancel this child.")
+    }
+
+    println("I am first, I want to wait until my child being finishing.")
+    //An outer coroutine (runBlocking in our example) does not complete until all the coroutines launched in its scope complete.
+    //The main difference between runBlocking and coroutineScope is that the latter does not block the current thread while waiting for all children to complete.
+}
+
+//https://sourcegraph.com/github.com/Kotlin/kotlinx.coroutines@0.26.0-eap13/-/blob/coroutines-guide.md#making-computation-code-cancellable
+fun patientWaitUntilCancelHeavyJob() = runBlocking {
+    val bkTaskDuration = 1000L
+    val cancelDuration = 1300L
+    val longJobDuration = 5000L
+
+    launch {
+        //This is my child(runBlocking main)
+        delay(bkTaskDuration)
+        println("messaging...")
+    }
+
+    coroutineScope {
+        //An other long-time blocking which doesn't complete until child finishes.
+        //A new scope which blocks main(runBlocking).
+        val child = this.launch(Dispatchers.Default) {
+            //A child of the new scope
+            var itor = 100000000000000
+
+            try {
+                while (itor >= 0 && isActive) { //Remove isActive, the job is so heavy and cannot be cancelled.
+                    println("receiving...$itor")
+                    itor--
+                }
+            } finally {
+                withContext(NonCancellable) {
+                    //Without this, the delay(5000)will block finally{}, and no println(), however, the finally will be cancelled.
+                    delay(longJobDuration) //This is a suspend functions(blocking) which can do a bit long.
+                    println("final receiving...$itor")
+                }
+            }
+        }
+        println("The child in the new scope.")
+        delay(cancelDuration)
+        child.cancel()
+        println("Wait for stop.")
         child.join()
         println("I have no patient, cancel this child.")
     }
