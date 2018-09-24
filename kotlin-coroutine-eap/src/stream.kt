54f7c78bdf952b53560018e24ca6998c0ccfb2cf
XinyueZ <xinyue.zhao@immonet.de>
2018-09-24 15:03:24 +0200

    [SECTION]Building channel producers, observe-consumer or producer-consumer
    https://sourcegraph.com/github.com/Kotlin/kotlinx.coroutines@0.26.1/-/blob/coroutines-guide.md#building-channel-producers

--- kotlin-coroutine-eap/src/stream.kt
+++ kotlin-coroutine-eap/src/stream.kt
@@ -1,11 +1,19 @@
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.consumeEach
+import kotlinx.coroutines.channels.produce
+import kotlinx.coroutines.currentScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 fun main(args: Array<String>) {
     //runChannel()
-    runAndCloseChannel()
+    //runAndCloseChannel()
+
+    dataProducer1()
+    print("\n---2nd way---\n")
+    dataProducer2()
 }
 
 // https://sourcegraph.com/github.com/Kotlin/kotlinx.coroutines@0.26.1/-/blob/coroutines-guide.md#channel-basics
@@ -39,4 +47,43 @@ fun runAndCloseChannel() = runBlocking{
         print("$rec ") //Receive can block(suspend).
     }
     println("\nend")
+}
+
+// https://sourcegraph.com/github.com/Kotlin/kotlinx.coroutines@0.26.1/-/blob/coroutines-guide.md#building-channel-producers
+fun dataProducer1() = runBlocking {
+    val producer = createData()
+    producer.consumeEach {//consumeEach can block(suspend).
+        print("$it ")
+    }
+    println("\nend")
+}
+
+// https://sourcegraph.com/github.com/Kotlin/kotlinx.coroutines@0.26.1/-/blob/coroutines-guide.md#building-channel-producers
+fun dataProducer2() = runBlocking {
+    val producer = provideData()
+
+    launch {
+        producer.consumeEach {//consumeEach can block(suspend).
+            print("$it ")
+        }
+        println("\nend")
+    }
+
+    println("Output:")
+}
+
+private fun CoroutineScope.createData() = this.produce<Int> {
+    (0..100).forEach { it ->
+        delay(100)
+        channel.send(it)
+    }
+}
+
+private suspend fun provideData() = currentScope {
+    this.produce<Int> {
+        (0..100).forEach { it ->
+            delay(100)
+            channel.send(it)
+        }
+    }
 }
