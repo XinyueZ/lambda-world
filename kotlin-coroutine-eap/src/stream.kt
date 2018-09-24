8468a07c2fb9c949e86df251303dae7d82b03575
XinyueZ <xinyue.zhao@immonet.de>
2018-09-24 14:15:09 +0200

    [SECTION]Closing and iteration over channels, if no close, the for on channel waits ever
    https://sourcegraph.com/github.com/Kotlin/kotlinx.coroutines@0.26.1/-/blob/coroutines-guide.md#closing-and-iteration-over-channels

--- kotlin-coroutine-eap/src/stream.kt
+++ kotlin-coroutine-eap/src/stream.kt
@@ -4,7 +4,8 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 fun main(args: Array<String>) {
-    runChannel()
+    //runChannel()
+    runAndCloseChannel()
 }
 
 // https://sourcegraph.com/github.com/Kotlin/kotlinx.coroutines@0.26.1/-/blob/coroutines-guide.md#channel-basics
@@ -18,6 +19,24 @@ fun runChannel() = runBlocking {
     }
     (0..100).forEach {_ ->
         print("${channel.receive()} ") //Receive can block(suspend).
+    }
+    println("\nend")
+}
+
+// https://sourcegraph.com/github.com/Kotlin/kotlinx.coroutines@0.26.1/-/blob/coroutines-guide.md#closing-and-iteration-over-channels
+fun runAndCloseChannel() = runBlocking{
+    val channel = Channel<Int>()
+    launch {
+        (0..100).forEach { it ->
+            delay(100)
+            channel.send(it)
+        }
+        channel.close() //A "close" end-token will be sent and "for" below understands it.
+        //If the close() is not called, you will never see "end", the "for" waits end-token forever.
+    }
+
+    for( rec in channel){
+        print("$rec ") //Receive can block(suspend).
     }
     println("\nend")
 }
